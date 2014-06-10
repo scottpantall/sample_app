e20f5503720ceb524a6a335ada5fc0ed94133f6c
Scott Pantall <aspantall@gmail.com>
2014-06-09 19:15:41 -0600

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,6 +34,22 @@ describe "Static pages" do  #All tests go in this loop
       click_link "sample app"
       expect(page).to have_title(full_title(''))
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
 	end
   
 	describe "Help page" do
